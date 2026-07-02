c2ffb06b24a0915881ace7866781275aae304553
iki1uc <entertrainer@web.de>
2026-07-02 22:36:09 +0200

    Add TMP_from_SUB function to transform sub object

--- TMP/tmp.js
+++ TMP/tmp.js
@@ -1,1 +1,11 @@
+export function TMP_from_SUB(sub) {
+  return {
+    id: sub.id,
+    alias: sub.alias,
+    vector: sub.vector,
+    wake: sub.wake,
+    pipeline: sub.pipeline,
+    tmp: "TMP_" + sub.id
+  };
+}
 
